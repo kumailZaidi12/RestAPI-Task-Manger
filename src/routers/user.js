d2902976bd61f4f64db552c54186b980fed220c8
kumailZaidi12 <zaidikumail12@gmail.com>
2020-10-11 11:43:09 +0530

    upload and delete profile pictures

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,7 @@
 const express=require('express')
 const User=require('../model/user')
+const multer = require('multer')
+const sharp = require('sharp')
 const auth =require('../middleware/auth')
 const router = new express.Router()
 
@@ -97,6 +99,48 @@ router.delete('/users/me',auth, async (req,res) =>{
     }
 
 
+})
+
+
+const upload = multer({
+    limits :{
+        fileSize : 1000000
+    },
+    fileFilter(req,file,callback){
+        if(!file.originalname.match(/\.(jpg|png|jpeg)$/)){
+            return callback(new Error('Please upload an image'))
+        }
+        callback(undefined,true)
+    }
+})
+
+router.post('/users/me/avatar',auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250
+    }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+   }, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar',auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar) {
+        throw new Error()
+    }
+    res.set('Content-Type', 'image/png')
+    res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 module.exports=router
